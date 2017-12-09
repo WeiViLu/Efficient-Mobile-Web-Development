1d064adf0f53c2ad9829df49efedf0337d1d2cd4
陆薇薇 <WeiViLu@outlook.com>
2017-12-09 21:02:04 +0800

    ‘第三章’

---
+++ 第二章/node.js
@@ -0,0 +1,12 @@
+// node.js官方实例
+const http=require('http');
+const hostname='127.0.0.1';
+const port=3000;
+const server=http.createServer((req,res)=>{
+    res.statusCode=200;
+    res.setHeader('Content-Type','Text/plain');
+    res.end('Hello world\n');
+});
+server.listen(port,hostname,()=>{
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
