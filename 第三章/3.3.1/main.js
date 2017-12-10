d0c5f720e4ac3039eabafbad781d558e93166316
陆薇薇 <WeiViLu@outlook.com>
2017-12-11 07:26:09 +0800

    ‘第三章’

--- 第三章/3.3.1/main.js
+++ 第三章/3.3.1/main.js
@@ -1,28 +1,33 @@
 
-//获取记录内容的文本域
-var el=document.querySelector('#content');
-el.addEventListener('blur',function(){
-	var data=el.value;
-	if(navigator.online){
+// 获取记录内容的文本域
+var el = document.querySelector('#content');
+// 为文本域DOM节点添加blur事件
+el.addEventListener('blur', function(){
+	// 获取文本域的内容
+	var data = el.innerHTML;
+	// 如果是在线状态，就直接保存到服务器
+	if(navigator.onLine){
 		saveOnline(data);
 	}else{
-		localStorage.setItem('data',data);
+		// 如果是离线状态，则保存到本地
+		localStorage.setItem('data', data);
 	}
-})
-
-//监听上线事件
-window.online=function(){
-	var data=localStorage.getItem('data');
+});
+// 监听上线事件
+window.online = function(){
+	// 从本地存储获取数据
+	var data = localStorage.getItem('data');
 	if(!!data){
+		// 如果数据存在，则保存到服务器
 		saveOnline(data);
+		// 同时，清空本地的存储
 		localStorage.removeItem('data');
 	}
-}
-
-//保存内容
+};
+// 保存内容的具体代码
 function saveOnline(data){
-	var xhr=new XMLHttpRequest();
-	xhr.open('POST','http://localhost:8000/savedata');
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST', 'http://localhost:8000/savedata');
 	xhr.send('data='+data);
 }
 
--- 第三章/3.3.1/sw.js
+++ 第三章/3.3.1/sw.js
@@ -1,13 +1,14 @@
 // 需要缓存的资源列表
-var cacheFiles = [
+var cacheFiles=[
     'style.css',
     'main.js'
 ];
+
 // 在install事件里缓存资源
-self.addEventListener('install', function (evt) {
+self.addEventListener('install',function(evt){
     evt.waitUntil(
-        caches.open('mycache').then(function (cache) {
+        catches.open('mycache').then(function(cache){
             return cache.addAll(cacheFiles);
         })
-    );
-});
+    )
+})
